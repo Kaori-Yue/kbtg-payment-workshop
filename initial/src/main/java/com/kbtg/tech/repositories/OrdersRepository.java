bd59794b59b9ed2ca387c3b89dea7faf26d09082
cpsu <everdayforlove@gmail.com>
2020-12-16 12:54:45 +0700

    finish

---
+++ initial/src/main/java/com/kbtg/tech/repositories/OrdersRepository.java
@@ -0,0 +1,9 @@
+package com.kbtg.tech.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.kbtg.tech.dto.*;
+
+public interface OrdersRepository extends CrudRepository<Order, Long> {
+    
+}
